cd1c3be69d6d14af6073f2a93a9094bf356bdfb5
Kouki Nakayama <k.number.02@gmail.com>
2022-03-12 22:24:46 +0900

    [add] todolistの題名表示

---
+++ todo-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import data from './data.json';
+import React, { useState } from 'react';
+// components
+import Header from './header';
+import TaskList from './TaskList';
+function App() {
+  const [ taskList, setTaskList ] = useState(data);
+  return (
+    <div className="App">
+      <Header/>
+      <TaskList taskList={taskList}/>
+    </div>
+  );
+}
+
+export default App;
